47e8fe140ddd7fd791c89a25ee9ed79d1b9e73f4
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2023-09-11 19:19:17 -0400

    Clone template with depth 1

--- yuca/template/template_app.py
+++ yuca/template/template_app.py
@@ -11,24 +11,29 @@ from yuca.app_data import AppData
 template_app = typer.Typer()
 
 
-def get_repo_name_from_url(url: str) -> str:
-    last_slash_index = url.rfind("/")
-    last_suffix_index = url.rfind(".git")
-    if last_suffix_index < 0:
-        last_suffix_index = len(url)
+def get_repo_name_from_url(url: str) -> str | None:
+    _from = url.rfind("/")
+    _to = url.rfind(".git")
+    if _to < 0:
+        _to = len(url)
 
-    if last_slash_index < 0 or last_suffix_index <= last_slash_index:
-        raise Exception("Badly formatted url {}".format(url))
+    if _from < 0 or _to <= _from:
+        return None
 
-    return url[last_slash_index + 1 : last_suffix_index]
+    return url[_from + 1 : _to]
 
 
 def _resolve_git_template(url: str, name: str | None):
     wh_folder = Path(AppData.get_default_warehouse())
     templates_folder = wh_folder / "templates"
     os.chdir(str(templates_folder))
+
     template_name = get_repo_name_from_url(url) if name is None else name
-    Repo.clone_from(url, to_path=str(templates_folder / template_name))
+    if template_name is None:
+        print("Invalid url {url}")
+        return
+
+    Repo.clone_from(url, to_path=str(templates_folder / template_name), depth=1)
 
     base_recipe = templates_folder / template_name / "base-recipe.yml"
     if base_recipe.exists():
