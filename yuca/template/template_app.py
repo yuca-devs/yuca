8a19758aa69e25b0f24238d31ad8a12ba56e1267
Gustavo Viera López <gvieralopez@gmail.com>
2023-09-14 09:34:26 +0200

    Refactor template app

--- yuca/template/template_app.py
+++ yuca/template/template_app.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import logging
 from pathlib import Path
 from typing import Annotated, Optional
 
@@ -23,30 +24,17 @@ def get_repo_name_from_url(url: str) -> str | None:
     return url[_from + 1 : _to]
 
 
-def _resolve_git_template(url: str, name: str | None):
+def _resolve_git_template(url: str, destination_path: Path):
+    Repo.clone_from(url, to_path=str(destination_path), depth=1)
+
+def _copy_base_recipe(template_path: Path):
     wh_folder = Path(AppData.active_warehouse())
-    templates_folder = wh_folder / "templates"
-    os.chdir(str(templates_folder))
-
-    template_name = get_repo_name_from_url(url) if name is None else name
-    if template_name is None:
-        print("Invalid url {url}")
-        return
-
-    final_template_path = templates_folder / template_name
-    if final_template_path.exists():
-        print(f"Template '{template_name}' already exists in your active warehouse")
-        return
-
-    Repo.clone_from(url, to_path=str(final_template_path), depth=1)
-
-    base_recipe = templates_folder / template_name / "base-recipe.yml"
+    base_recipe = template_path / "base-recipe.yml"
     if base_recipe.exists():
         shutil.copyfile(
             str(base_recipe),
-            str(wh_folder / "recipes" / f"{template_name}-base-recipe.yml"),
+            str(wh_folder / "recipes" / f"{template_path.name}-base-recipe.yml"),
         )
-
 
 def _resolve_zip_template(url: str, name: str | None):
     raise NotImplementedError()
@@ -54,9 +42,29 @@ def _resolve_zip_template(url: str, name: str | None):
 
 @template_app.command("get", help="Download a yuca template from a url")
 def template_get(url: str, name: Annotated[Optional[str], typer.Option()] = None):
+    # Resolve the template name to be used locally
+    template_name = get_repo_name_from_url(url) if name is None else name
+    if template_name is None:
+        logging.error("Invalid url {url}")
+        return
+
+    # Go to the templates directory
+    templates_folder = Path(AppData.active_warehouse()) / "templates"
+    os.chdir(str(templates_folder))
+
+    # Check that there are not other templates with the same name  
+    final_template_path = templates_folder / template_name
+    if final_template_path.exists():
+        logging.error(f"Template '{template_name}' already exists in your active warehouse")
+        return
+    
+    # Download the template depending on the url type 
     if url.endswith(".git"):
-        _resolve_git_template(url, name)
+        _resolve_git_template(url, final_template_path)
     elif url.endswith(".zip"):
-        _resolve_zip_template(url, name)
+        _resolve_zip_template(url, final_template_path)
     else:
-        print(f"Invalid template url: '{url}'")
+        logging.error(f"Invalid template url: '{url}'")
+
+    # Copy base recipe to the recipes folder of the warehouse
+    _copy_base_recipe(final_template_path)
